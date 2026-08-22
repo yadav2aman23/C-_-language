80b4b53611643b37e24cca16ab633461ca910178
yadav2aman23 <yadaviactaman2030@gmail.com>
2026-08-22 09:45:33 +0000

    Aug 22, 2026, 3:15 PM

--- 1st.cpp
+++ 1st.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // This program calculates and prints the sum of 5 and 6.
 int main() {
+    int a;
     int sum = 5 + 6;
     cout << "Sum: " << sum << endl;
     cout <<"This is name ";
