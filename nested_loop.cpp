8f063456cb96d2111cbac416103a2a625fc1934e
yadav2aman23 <yadaviactaman2030@gmail.com>
2026-08-25 13:58:54 +0000

    Aug 25, 2026, 7:28 PM

---
+++ nested_loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10,b=15;
+    if(a==10){
+        if(b==15){
+            cout<<"value od a =10 and valur of b=15";
+        
+        }
+    
+    }
+}
