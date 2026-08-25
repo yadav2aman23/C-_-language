7ac86b3d2ad4e75d980d840d669962862582d445
yadav2aman23 <yadaviactaman2030@gmail.com>
2026-08-25 14:19:03 +0000

    Aug 25, 2026, 7:49 PM

--- leap_yeras.cpp
+++ leap_yeras.cpp
@@ -1,27 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int years;
-    cout<<"Enter the yeras : ";
-    cin>>years;
-    if(years%4==0)
-    if(years%100==0){
-        cout <<"It is leep yeras ";
-    }else{
-        cout <<"it is not leep yeras";
-    }
-    if(years%400==0){
-        cout <<"It is leep yeras ";
-    }
-    else{
-        cout<< "it is not leep yeras ";
-    }
-    {
-        cout <<"It is leep yeras";
-    }
-    
+    int yeras;
+    cout<<"Enter the your years: ";
+    cin >>yeras;
 
-        cout <<"It is not leep yeras";
-
-    return 0;
+    if(yeras%4==0){
+        
+    }
 }
