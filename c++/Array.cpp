123d4d7ba34ad41e4fe10fbc78b6f476f02196dc
yadav2aman23 <yadaviactaman2030@gmail.com>
2026-08-26 13:56:47 +0000

    Aug 26, 2026, 7:26 PM

--- c++/Array.cpp
+++ c++/Array.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int num[]={10,20,-96,-10,40,88,-89}
-    int size=7;
+    int num[]={10,20,-96,-10,40,88}
+    int size=6;
     int smallest=INT_MAX;
-    for(int i=0; i<size;i++)
-    {
+    for(int i=0; i<size; i++)    {
         if(num[i]<smallest){
             smallest=num[i];
 
