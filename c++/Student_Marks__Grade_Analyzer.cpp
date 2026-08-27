b4cd86a17fbcefc3134ec3ef8516338b6729dbf8
yadav2aman23 <yadaviactaman2030@gmail.com>
2026-08-27 17:39:44 +0000

    Aug 27, 2026, 11:09 PM

--- c++/Student_Marks__Grade_Analyzer.cpp
+++ c++/Student_Marks__Grade_Analyzer.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     int stud=4;
     int std_sub=4;
+    int marks[4][4];
     string stud_name[4];
     string stud_sub[4];
     for(int i=0; i<4;i++){
@@ -15,8 +16,12 @@ int main(){
     for(int i=0;i<4;i++){
         cout << "Enter the student subject name = "<< i+1 <<":";
         cin >> stud_sub[i];
+
+    }
+    for(int i=0;i<4;i++){
         
     }
+
 
     return 0;
 
