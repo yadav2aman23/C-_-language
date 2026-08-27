6429b4ad265efcd95185bce50481bc9e76c3b93c
yadav2aman23 <yadaviactaman2030@gmail.com>
2026-08-27 04:33:06 +0000

    Aug 27, 2026, 10:03 AM

--- c++/CheckTheVowels.cpp
+++ c++/CheckTheVowels.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
-void vowl(char x)
-{
-    if (x=='a'|| )
+int main(){
+    int x;
+    cout << "enter the number:  ";
+    cin >>x;
+    if(x%1==0 || x%x==0){
+        cout<<"This is prime number :";
+
+    }
+    else{
+        cout << "It is not prime number :";
+    }
+    return 0;
 }
