8e66213cf4e9ea833f04661747143b03cee1da7a
yadav2aman23 <yadaviactaman2030@gmail.com>
2026-08-27 06:12:46 +0000

    Aug 27, 2026, 11:42 AM

--- c++/Student_Marks__Grade_Analyzer.cpp
+++ c++/Student_Marks__Grade_Analyzer.cpp
@@ -5,5 +5,20 @@ int main(){
     int subject=4;
     string Student_name[4];
     string student_Subject_name[4];
-    
+    //for studnet name ;
+    for(int i=0; i<4;i++)
+    {
+
+        cout << "Enter the student name : "<<i+1<<":";
+        cin >>Student_name[i];
+
+    }
+    //for student sub name ;
+    for(int i=0; i<4;i++){
+        cout<<"Enter the student_subject_code= "<<i+1<<":";
+        cin >>student_Subject_name[i];
+
+    }
+return 0;
+
 }
