362a98d08d268537337a52545c0aaebbfe78fb73
yadav2aman23 <yadaviactaman2030@gmail.com>
2026-08-27 04:39:15 +0000

    Aug 27, 2026, 10:09 AM

--- c++/CheckTheVowels.cpp
+++ c++/CheckTheVowels.cpp
@@ -4,7 +4,14 @@ int main(){
     int x;
     cout << "enter the number:  ";
     cin >>x;
-    if(x<=1)
+    if(x<=1){
+        if(x%x==0){|
+        cout << "This is prime ";
+    }
+    }else
+    {
+        cout
+    }
    
     }
     return 0;
